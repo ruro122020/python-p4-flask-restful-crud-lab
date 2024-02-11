88c7ede07ee59f95c7703c269bf2aaf78d6f1a25
Ruth <ruro122020@gmail.com>
2024-02-11 16:01:41 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -46,6 +46,24 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        if plant:
+          update_plant = request.get_json()
+          for attr in update_plant:
+              setattr(plant, attr, update_plant.get(attr))
+          db.session.add(plant)
+          db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
